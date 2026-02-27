be77e038b82b885698b728bf57dc39d06e5d6a17
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2026-02-27 09:14:08 +0100

    Mise à jour

---
+++ backend.mjs
@@ -0,0 +1,45 @@
+import PocketBase from 'pocketbase';
+
+const db = new PocketBase("http://127.0.0.1:8090");
+
+export async function getOffres() {
+    try {
+        let data = await db.collection('Maison').getFullList({
+            sort: '-created',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function getOffre(id) {
+    try {
+        let data = await db.collection('Maison').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function getImageUrl(record, recordImage) {
+    return db.files.getURL(record, recordImage);
+}
+
+export async function addOffre(house) {
+    try {
+        await db.collection('Maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
