27d1f536a493694fa1d05e1826f5c363799e69c8
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2026-03-06 09:59:46 +0100

    ajout d'une page agents dans le header, mes agents du pocketbase apparait, maie je n'ai pas réussi a faire le favori pour avec l'etoiles en vaint. comit et push de fin de seance

--- backend.mjs
+++ backend.mjs
@@ -43,3 +43,62 @@ export async function addOffre(house) {
         };
     }
 }
+
+export async function getAgent(id) {
+    try {
+        const data = await pb.collection('agent').getOne(id, {
+                expand: 'maison',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant l\'agent', error);
+        return null;
+    }
+}
+
+
+export async function getAgents() {
+    try {
+        const data = await db.collection('agent').getFullList();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant l\'agent', error);
+        return null;
+    }
+}
+
+export async function getOffresByAgent(agentId) {
+    try {
+        const data = await db.collection('maison').getFullList({
+            sort: '-created',
+            filter: `agent = "${agentId}"`,
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en filtrant par agent', error);
+        return [];
+    }
+}
+
+export async function addAgent(formData) {
+    try {
+        await db.collection('agent').create(formData);
+        return { success: true, message: 'Agent ajouté avec succès' };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant l\'agent', error);
+        return { success: false, message: 'Une erreur est survenue en ajoutant l\'agent' };
+    }
+}
+
+/*export async function setFavori(house) {
+    try {
+        await pb.collection('maison').update(house.id, { favori: !house.favori });
+    } catch (error) {
+        console.log('Une erreur est survenue en mettant à jour le favori', error);
+    }
+}*/
+
+export async function setFavori(Maison) {
+    await db.collection('maison').update(Maison.id, {favori: !Maison.favori});
+}
